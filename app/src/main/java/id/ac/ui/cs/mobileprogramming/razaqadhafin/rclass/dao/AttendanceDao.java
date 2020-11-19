16c5cc498f3ef0652d31a5c0bdd51fdb7dda8e12
razaqa <dhafins18@gmail.com>
2020-11-19 23:36:42 +0700

    Add Dao for dashboard

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/rclass/dao/AttendanceDao.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.mobileprogramming.razaqadhafin.rclass.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+import id.ac.ui.cs.mobileprogramming.razaqadhafin.rclass.entity.Attendance;
+
+@Dao
+public interface AttendanceDao {
+
+    @Query("SELECT MAX(id) FROM attendances")
+    int getMaxId();
+
+    @Query("SELECT * FROM attendances WHERE id = :id")
+    LiveData<Attendance> getAttendanceById(int id);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(List<Attendance> attendances);
+
+    @Query("UPDATE attendances SET is_present = :isPresent WHERE id = :id")
+    void updateIsPresent(int isPresent, int id);
+}
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/razaqadhafin/rclass/dao/ClassroomDao.java
@@ -0,0 +1,44 @@
+package id.ac.ui.cs.mobileprogramming.razaqadhafin.rclass.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Transaction;
+
+import java.util.List;
+
+import id.ac.ui.cs.mobileprogramming.razaqadhafin.rclass.entity.Classroom;
+
+@Dao
+public interface ClassroomDao {
+
+    @Query("SELECT MAX(id) FROM classrooms")
+    int getMaxId();
+
+    @Query("SELECT * FROM classrooms")
+    LiveData<List<Classroom>> getAllClassrooms();
+
+    @Query("SELECT * FROM classrooms where name = :name LIMIT 1")
+    LiveData<Classroom> getClassroomByName(String name);
+
+    @Query("SELECT SUM(present_count) FROM classrooms")
+    int getTotalPresentCount();
+
+    @Query("SELECT SUM(absent_count) FROM classrooms")
+    int getTotalAbsentCount();
+
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Classroom classroom);
+
+    @Query("UPDATE classrooms SET present_count = :presentCount WHERE id = :id")
+    void updatePresentCount(int presentCount, int id);
+
+    @Query("UPDATE classrooms SET absent_count = :absentCount WHERE id = :id")
+    void updateAbsentCount(int absentCount, int id);
+
+    @Query("UPDATE classrooms SET last_attendance_id = :last_attendance_id WHERE id = :id")
+    void updateLastAttendanceId(int last_attendance_id, int id);
+}
